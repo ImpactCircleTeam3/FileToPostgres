622589e96d1619d5c2fba268ebe31705ff528ee2
JangasCodingplace <janisgoesser92@gmail.com>
2021-11-28 01:17:59 +0100

    Prepare project and code skeletone

--- main.py
+++ main.py
@@ -1,16 +1,109 @@
-# This is a sample Python script.
+import psycopg2
+import os
+import json
+import ast
+import csv
+from typing import Optional, List
+from dataclasses import dataclass
+from dotenv import load_dotenv
+from datetime import datetime
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Settings:
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+    @staticmethod
+    def get_db_kwargs() -> dict:
+        return {
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "host": os.getenv("DB_HOST"),
+            "port": int(os.getenv("DB_PORT")),
+            "database": os.getenv("DB_NAME")
+        }
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class DB:
+    DB: Optional["DB"] = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @classmethod
+    def get_instance(cls) -> "DB":
+        if not cls.DB:
+            cls.DB = cls()
+        return cls.DB
+
+    def __init__(self):
+        self._connect()
+
+    def _connect(self):
+        self.conn = psycopg2.connect(**Settings.get_db_kwargs())
+        self.cur = self.conn.cursor()
+        self.conn.autocommit = True
+
+
+@dataclass
+class Sink:
+    status_id: int
+    text: str
+    url: str
+    favorite_count: int
+    retweet_count: int
+    trend: str
+    normalized_trend: str
+    language_code: str
+    hashtags: List[str]
+    tagged_persons: List[str]
+    time_collected: datetime
+    date_label: datetime
+
+
+@dataclass
+class Source:
+    index: str
+    status_id: str
+    status_url: str
+    tweet_query: str
+    text: str
+    favorite_count: str
+    retweet_count: str
+    trend: str
+    normalized_trend: str
+    time_collected: str
+    date_label: str
+    language_code: str
+    hashtags: str
+    tagged_persons: str
+
+    def parse_to_sink(self) -> Sink:
+        return Sink(
+            status_id=json.loads(self.status_id),
+            text=self.text,
+            url=self.status_url,
+            favorite_count=int(self.favorite_count),
+            retweet_count=int(self.retweet_count),
+            trend=self.trend,
+            normalized_trend=self.normalized_trend,
+            language_code=self.language_code,
+            hashtags=[] if self.hashtags == "" else ast.literal_eval(self.hashtags),
+            tagged_persons=[] if self.tagged_persons == "" else ast.literal_eval(self.tagged_persons),
+            time_collected=datetime.strptime(self.time_collected, "%Y-%m-%dT%H:%M:%SZ"),
+            date_label=datetime.strptime(self.date_label, "%b %d %Y")
+        )
+
+
+def extract_and_transform_source() -> List[Sink]:
+    data_path = os.path.join(Settings.BASE_DIR, "data", "greener_tweets_cleaned.csv")
+    data = []
+    with open(data_path, mode="r") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for row in reader:
+            source = Source(*row)
+            data.append(source.parse_to_sink())
+    return data
+
+
+if __name__ == "__main__":
+    extract_and_transform_source()
